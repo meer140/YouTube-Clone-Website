16bddc6c535c27a0991ba688c9b82c46f6e16527
meer140 <mirmustafa2005ali@gmail.com>
2026-06-19 02:30:42 +0500

    utils code

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,18 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import express from "express"
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-connectDB();
+connectDB()
+.then(()=>{
+    let port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log("Server is listening at port : ", port);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection error.")
+})
 
 
 
